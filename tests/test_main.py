f61054a48b4a7b3ad904552c94019b64e5813f4e
wuyihan <wuyihan@example.com>
2026-04-21 11:36:17 +0800

    2026/4/21/11:36

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,21 @@
-from demo_project.main import f
+from demo_project.main import f, g, h
 
 
-def test() -> None:
+def test1() -> None:
     assert f(90) == "A"
+
+
+def test2() -> None:
+    assert g("88") == 88
+
+
+def test3() -> None:
+    assert h(11) is True
+
+
+def test4() -> None:
+    assert h(-11) is False
+
+
+def test5() -> None:
+    assert h(150) is False
