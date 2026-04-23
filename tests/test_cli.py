31fd9d61b87f9140ccd2705e3e73e7d884d5dd73
wuyihan <wuyihan@example.com>
2026-04-23 10:34:15 +0800

    2026/4/23/10:33

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,31 +3,15 @@ from demo_project.cli import k
 
 def test1(monkeypatch, capsys) -> None:
     monkeypatch.setattr("builtins.input", lambda _: "q")
-    c = k()
-    d = capsys.readouterr()
-    assert c is True
-    assert "it's over" in d.out
+    a = k()
+    b = capsys.readouterr()
+    assert a is True
+    assert "it's over" in b.out
 
 
 def test2(monkeypatch, capsys) -> None:
-    monkeypatch.setattr("builtins.input", lambda _: "abc")
-    c = k()
-    d = capsys.readouterr()
-    assert c is False
-    assert "Invalid input. please enter a number." in d.out
-
-
-def test3(monkeypatch, capsys) -> None:
-    monkeypatch.setattr("builtins.input", lambda _: "197")
-    c = k()
-    d = capsys.readouterr()
-    assert c is False
-    assert "Invalid input. please enter a number from 0 to 100." in d.out
-
-
-def test4(monkeypatch, capsys) -> None:
-    monkeypatch.setattr("builtins.input", lambda _: "11")
-    c = k()
-    d = capsys.readouterr()
-    assert c is False
-    assert "score: 11, grade: E" in d.out
+    monkeypatch.setattr("builtins.input", lambda _: "111")
+    a = k()
+    b = capsys.readouterr()
+    assert a is False
+    assert "Invalid input.please enter a number from 0 to 100."
