9014937eb25ee64e4f790dcba6792707bfe00154
wuyihan <wuyihan@example.com>
2026-04-26 11:48:43 +0800

    2026/4/26/11:48

---
+++ tests/test_storage.py
@@ -0,0 +1,17 @@
+from demo_project.storage import scores_load, scores_save
+
+
+def test1(tmp_path, monkeypatch) -> None:
+    monkeypatch.setattr("demo_project.storage.SCORE_FILE", tmp_path / "scores.json")
+    a = [11, 12, 13, 14]
+    scores_save(a)
+    b = scores_load()
+    assert b == a
+
+
+def test2(tmp_path, monkeypatch) -> None:
+    monkeypatch.setattr("demo_project.storage.SCORE_FILE", tmp_path / "score.json")
+    a = [11, 12, 13, 14]
+    scores_save(a)
+    b = scores_load()
+    assert b == a
