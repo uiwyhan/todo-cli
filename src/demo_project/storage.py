00a25c21b7979b734f5d140ae50d111ec752828b
wuyihan <wuyihan@example.com>
2026-04-30 22:43:35 +0800

    2026/4/30/22:43

--- src/demo_project/storage.py
+++ src/demo_project/storage.py
@@ -1,23 +1,21 @@
 import json
 from pathlib import Path
-
 from demo_project.models import s
 
 
 def save_storage(x: Path, y: list) -> None:
-    a = []
+    b = []
     for i in y:
-        b = {
-            "a": i.a,
-            "b": i.b,
-            "c": i.c,
-        }
-        a.append(b)
-    a = json.dumps(a, ensure_ascii=False, indent=3)
-    x.write_text(a, encoding="utf-8")
+        a = {"a": i.a, "b": i.b, "c": i.c}
+        b.append(a)
+    c = json.dumps(b, ensure_ascii=False, indent=4)
+    c.write_text(encoding="utf-8")
 
 
 def load_storage(x: Path) -> list:
+    if not x.exists():
+        print("there is no this Path")
+        return []
     a = []
     b = x.read_text(encoding="utf-8")
     c = json.loads(b)
@@ -29,8 +27,3 @@ def load_storage(x: Path) -> list:
         )
         a.append(d)
     return a
-
-
-a = load_storage(Path("aaa.json"))
-for i in a:
-    print(i.a, i.b, i.c)
