c34aed4a46804251b32434b9454adc59d78e5f3b
wuyihan <wuyihan@example.com>
2026-05-14 09:36:10 +0800

    2026/5/14/9:35

--- src/demo_project/main.py
+++ src/demo_project/main.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser, ArgumentTypeError
 from pathlib import Path
 
 from demo_project.models import s
@@ -8,18 +8,28 @@ from demo_project.storage import load_storatge, save_storage
 path = Path("aaaa.json")
 
 
+def score_type(x: str) -> int:
+    try:
+        a = int(x)
+    except ValueError as error:
+        raise ArgumentTypeError("please input a number") from error
+    if a < 0 or a > 100:
+        raise ArgumentTypeError("please input a number from 0 to 100")
+    return a
+
+
 def build_parser() -> ArgumentParser:
     a = ArgumentParser()
     b = a.add_subparsers(dest="command", required=True, help="subparser")
     c = b.add_parser("add", help="add")
     c.add_argument("name", help="name")
-    c.add_argument("score", type=int, help="score")
+    c.add_argument("score", type=score_type, help="score")
     b.add_parser("list", help="list")
     d = b.add_parser("delete", help="delete")
     d.add_argument("name", help="name")
     e = b.add_parser("change", help="change")
     e.add_argument("name", help="name")
-    e.add_argument("score", type=int, help="score")
+    e.add_argument("score", type=score_type, help="score")
     b.add_parser("average", help="average")
     b.add_parser("top_student", help="top student")
     return a
@@ -37,13 +47,13 @@ def handle_list(x: list[s]) -> None:
         print(f"the average is {b.b}")
 
 
-def handle_add(x: list[s], y: str, z: int) ->s:
+def handle_add(x: list[s], y: str, z: int) -> s:
     a = s(y, z)
     x.append(a)
     return a
 
 
-def handle_delete(x: list[s], y: str) -> s|None:
+def handle_delete(x: list[s], y: str) -> s | None:
     for i in x:
         if i.a == y:
             x.remove(i)
@@ -51,7 +61,7 @@ def handle_delete(x: list[s], y: str) -> s|None:
     return None
 
 
-def handle_change(x: list[s], y: str, z: int) -> s|None:
+def handle_change(x: list[s], y: str, z: int) -> s | None:
     for i in x:
         if i.a == y:
             i.b = z
@@ -65,44 +75,48 @@ def main() -> None:
     b = load_storatge(a)
     c = build_parser().parse_args()
     print("the command is: {c.command}")
-    if c.command == "add":
-        if c.name is None:
-            print("please input the name of the students")
-            return
-        elif c.score is None:
-            print("please enter the score of the student")
-            return
-        else:
-            d = handle_add(b, c.name, c.score)
-            save_storage(a, b)
-            print(f"{d} has been added")
-            return
-    elif c.command == "list":
-        handle_list(b)
-        return
-    elif c.command == "delete":
-        if c.name is None:
-            print("please enter a name")
-            return
-        else:
-            d1 = handle_delete(b, c.name)
-            if d1 is None:
-                print("there is no the student")
+    try:
+        if c.command == "add":
+            if c.name is None:
+                print("please input the name of the students")
+                return
+            elif c.score is None:
+                print("please enter the score of the student")
                 return
             else:
+                d = handle_add(b, c.name, c.score)
                 save_storage(a, b)
-                print("{d1} has beed deleted")
+                print(f"{d} has been added")
                 return
-    elif c.command == "change":
-        if c.name is None:
-            print("please input a name")
+
+        elif c.command == "list":
+            handle_list(b)
             return
-        else:
-            d1 = handle_change(b, c.name, c.score)
-            if d1 is None:
-                print("there is no the student")
+        elif c.command == "delete":
+            if c.name is None:
+                print("please enter a name")
                 return
             else:
-                save_storage(a, b)
-                print("{d1} has been deleted")
+                d1 = handle_delete(b, c.name)
+                if d1 is None:
+                    print("there is no the student")
+                    return
+                else:
+                    save_storage(a, b)
+                    print("{d1} has beed deleted")
+                    return
+        elif c.command == "change":
+            if c.name is None:
+                print("please input a name")
                 return
+            else:
+                d1 = handle_change(b, c.name, c.score)
+                if d1 is None:
+                    print("there is no the student")
+                    return
+                else:
+                    save_storage(a, b)
+                    print("{d1} has been deleted")
+                    return
+    except ArgumentTypeError as error:
+        print(error)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,40 +1,60 @@
-from demo_project.cli import handle_add,handle_list,handle_delete,handle_change
+import pytest
+from pytest import CaptureFixture
+from demo_project.main import (
+    build_parser,
+    handle_add,
+    handle_change,
+    handle_delete,
+    handle_list,
+)
 from demo_project.models import s
-def test_add() ->None:
+
+
+def test_add() -> None:
     a = [
-        s("wuyihan",88),
-        s("wuyiqian",77),
+        s("wuyihan", 88),
+        s("wuyiqian", 77),
     ]
-    handle_add(a,"wuyilin",99)
+    handle_add(a, "wuyilin", 99)
     assert len(a) == 3
     assert a[2].a == "wuyilin"
     assert a[2].b == 99
-    
 
-def test_delete()->None:
+
+def test_delete() -> None:
     a = [
-        s("wuyihan",88),
-        s("wuyiqina",99),
+        s("wuyihan", 88),
+        s("wuyiqina", 99),
     ]
-    b = handle_delete(a,"wuyihan")
+    b = handle_delete(a, "wuyihan")
     assert b is not None
     assert len(a) == 1
     assert b.a == "wuyihan"
     assert b.b == 88
-def test_change() ->None:
+
+
+def test_change() -> None:
     a = [
-        s("wuyihan",88),
-        s("wuyiqian",99),
+        s("wuyihan", 88),
+        s("wuyiqian", 99),
     ]
-    b = handle_change(a,"wuyihan",90)
+    b = handle_change(a, "wuyihan", 90)
     assert b is not None
     assert b.a == a[0].a
     assert b.b == 90
     assert a[0].b == 90
-def test_list(capsys) ->None:
-    a = [
-        s("wuyihan",99)
-    ]
+
+
+def test_list(capsys:CaptureFixture[str]) -> None:
+    a = [s("wuyihan", 99)]
     handle_list(a)
     b = capsys.readouterr()
     assert str(a[0]) in b.out
+
+
+def test_type_error() -> None:
+    a = build_parser()
+    with pytest.raises(SystemExit):
+        a.parse_args(["add", "wuyihan", "222"])
+    with pytest.raises(SystemExit):
+        a.parse_args(["add", "wuyiqina", "wwww"])
